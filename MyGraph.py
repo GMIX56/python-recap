11eab169c5ff43df41bf800d107235e61dfcfc41
Grant <grant_mixon1@baylor.edu>
2024-01-24 13:28:56 -0600

    updated gitignore and changed print statement

--- MyGraph.py
+++ MyGraph.py
@@ -1,9 +1,17 @@
+
 import matplotlib.pyplot as plt
 import numpy as np
+#in order for this to work you have to download the library
+#everytime you are starting a new project, you create a vitural enviroment and install those in you virtual environment
+#Step 1: Create Virtual Enviroment(PC use py)(MAC use python3)
+#Step 2: Activate Virtual Enviroment(.\myvenv\Scripts\activate)
+#Step 3: Install 3rd party library/module
 
 x = np.linspace(0, 20, 100)
 plt.plot(x, np.sin(x))
 
 plt.show()
 
-print("Hi there!")
+print("Hello World!")
+#once i have checked into repository one on left is what we changed
+#one on the right is the new version
